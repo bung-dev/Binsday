6621d60ab2a3ddf8ee0a5acb5d41f7c7c5d06a17
bung-dev <wndgh26@gmail.com>
2024-11-02 20:52:05 +0900

    feat: 프로젝트설정

---
+++ src/main/java/com/example/binsday/BinsdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.binsday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinsdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinsdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/binsday/BinsdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binsday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinsdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
